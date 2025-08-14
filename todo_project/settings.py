83ee286151233af826f74eb7a801a5ce107e2c41
Ayan <ajamal16112@ada.edu.az>
2025-08-15 02:17:45 +0400

    Set up Django REST Framework with JWT login, session login for web UI, and pagination for task list

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -128,3 +128,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# django rest framework 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",   
+        "rest_framework.authentication.SessionAuthentication",         
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}

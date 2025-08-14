97591ae0572000dc1b4d977199159f326e3ff5ee
Ayan <ajamal16112@ada.edu.az>
2025-08-15 02:15:05 +0400

    added API routes: /api/tasks/, /api/token/, /api/token/refresh/

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r"tasks", TaskViewSet, basename="task")
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include(router.urls)),
+]

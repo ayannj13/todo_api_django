1f616e372994d3fbd2788b5b527ae7bf63780a9a
Ayan <ajamal16112@ada.edu.az>
2025-08-15 19:13:33 +0400

    removed old MyTasksList route and used DRF @action for /api/tasks/my/

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from . import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import TaskViewSet
@@ -11,5 +10,4 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
-    path("tasks/my/", views.MyTasksList.as_view(), name="task-my"),
 ]
